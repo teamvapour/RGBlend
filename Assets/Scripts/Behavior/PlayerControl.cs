b505114c693ef68eae9552ef35490913d45aadee
Kamen Dimitrov <kamend@gmail.com>
2014-01-25 16:05:58 +0200

    Latest

--- Assets/Scripts/Behavior/PlayerControl.cs
+++ Assets/Scripts/Behavior/PlayerControl.cs
@@ -18,9 +18,13 @@ public class PlayerControl : MonoBehaviour {
 	public float enemyRadiusMin = 1000.0f;
 	public float enemyRadiusMax = 3000.0f; 
 
+	private float textureOffset = 0.25f;
+	private int numSprites = 3;
+
+	private bool isMoving = false;
 	// Use this for initialization
 	void Start () {
-	
+		StartCoroutine(UpdateSpriteAnimation());
 	}
 
 
@@ -50,19 +54,40 @@ public class PlayerControl : MonoBehaviour {
 		Vector3 velN = vel.normalized;
 
 		if((Mathf.Abs(hTranslation) < 0.001f) && (Mathf.Abs(vTranslation) < 0.001f)) {
+			isMoving = false;
 			rigidbody.velocity = Vector3.zero;
 			transform.eulerAngles = new Vector3(90,(lastRotation*180.0f)/Mathf.PI,0);
 		} else {
+			isMoving = true;
 			float rotation = Mathf.Atan2(velN.x, velN.z);
 			rigidbody.velocity = vel;
 			transform.eulerAngles = new Vector3(90,(rotation*180.0f)/Mathf.PI,0);
 			lastRotation = rotation;
+			UpdateSpriteAnimation();
 		}
 
 	}
+
+	IEnumerator UpdateSpriteAnimation() {
+
+		while(true) {
+			if(isMoving) {
+				Vector2 tv = renderer.material.GetTextureOffset("_MainTex");
+				
+				tv.x += textureOffset;
+				
+				if(tv.x> textureOffset*(numSprites-1)) {
+					tv.x = 0.0f;
+				}
+				renderer.material.SetTextureOffset("_MainTex",tv);
+			}
+			yield return new WaitForSeconds(0.25f);
+		}
+		
+	}
 	
 	void OnCollisionEnter(Collision col) {
-	//	Debug.Log ("Bang!");
+		
 	}
 }
 
